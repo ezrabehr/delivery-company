2af6b2512ac02595c673b9de9076700c5f8fbd1e
ezrabehr <ezra.behr@gmail.com>
2023-12-17 16:51:22 +0200

    modified the settings

--- package_delivery/package_delivery/settings.py
+++ package_delivery/package_delivery/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "user",
     "request",
+    "crispy_forms",
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "user.User"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
